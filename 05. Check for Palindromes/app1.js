450386c44ea4a75a2609c2c9fc0ee99bff7e034b
alex.paclin <alex.paclin@gmail.com>
2019-03-18 16:01:49 +0500

    05. Check for Palindromes

---
+++ 05. Check for Palindromes/app1.js
@@ -0,0 +1,30 @@
+// Check for Palindromes
+
+function palindrome(str) {
+    var front = 0;
+    var back = str.length - 1;
+
+    while (back > front) {
+        while (str[front].match(/[\W_]/)) {
+            front++;
+            continue;
+        }   
+        while (str[back].match(/[\W_]/)) {
+            back--;
+            continue;
+        }
+
+        if (str[front].toLowerCase() !== str[back].toLowerCase()) {
+            return false;
+        }
+
+        front++;
+        back--;
+    }
+
+    return true;
+}
+
+console.log(palindrome("Civic"));
+console.log(palindrome("Step on no pets"));
+console.log(palindrome("No lemon, no melon"));
