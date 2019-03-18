f12cd8ba56bcd9a828007347e51f62bcea619ff9
alex.paclin <alex.paclin@gmail.com>
2019-03-18 11:15:48 +0500

    preparation

--- 02. Find the Longest Word in a String/app1.js
+++ 02. Find the Longest Word in a String/app1.js
@@ -1,3 +1,7 @@
+// Find the longest word in a string
+// Ex. The quick brown fox jumped over the lazy dog
+// Ex. What if we try a super-long word such as otorhinolaryngology
+
 function findLongestWord(str) {
     var words = str.split(' ');
     var maxLength = 0;
@@ -9,4 +13,5 @@ function findLongestWord(str) {
     return maxLength;
 }
 
-console.log(findLongestWord('asd asdf qwerty1 ff ddddd'));
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('What if we try a super-long word such as otorhinolaryngology'));
--- 02. Find the Longest Word in a String/app2.js
+++ 02. Find the Longest Word in a String/app2.js
@@ -1,3 +1,7 @@
+// Find the longest word in a string
+// Ex. The quick brown fox jumped over the lazy dog
+// Ex. What if we try a super-long word such as otorhinolaryngology
+
 function findLongestWord(str) {
     return str.split(' ').reduce(function(x, y) {
         return Math.max(x, y.length);
@@ -6,4 +10,5 @@ function findLongestWord(str) {
     return maxLength;
 }
 
-console.log(findLongestWord('asd asdf qwerty ff ddddd'));
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('What if we try a super-long word such as otorhinolaryngology'));
