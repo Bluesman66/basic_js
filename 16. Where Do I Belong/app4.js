a7dbb6a6547d990260c3afde31b53d9f410ea007
alex.paclin <alex.paclin@gmail.com>
2019-03-21 16:01:51 +0500

    16. Where Do I Belong

---
+++ 16. Where Do I Belong/app4.js
@@ -0,0 +1,16 @@
+// Where Do I Belong
+// ([1, 2, 3, 4], 1.5) returns 1 because 1.5 > 1 && 1.5 < 2
+// ([20, 3, 5], 19) returns 2 because in sorted array 19 > 5 && 19 < 20
+// ([40, 60], 50) returns 1 because 50 > 40 && 50 < 60
+// ([40, 60], 500) returns 2 because 500 > 60 (2 is a length of array)
+
+function getIndexToIns(arr, num) {
+    var index = arr.sort((curr, next) => curr > next)
+        .findIndex((currNum) => num <= currNum);
+    return index === -1 ? arr.length : index;    
+}
+
+console.log(getIndexToIns([1, 2, 3, 4], 1.5));
+console.log(getIndexToIns([20, 3, 5], 19));
+console.log(getIndexToIns([40, 60], 50));
+console.log(getIndexToIns([40, 60], 500));
